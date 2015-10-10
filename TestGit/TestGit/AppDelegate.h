983e56711d9be806eea7cf82f72bf88741b2b335
Ankit <development.sunfocus@gmail.com>
2015-10-10 13:47:02 +0530

    Create TestGit Project

---
+++ TestGit/TestGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestGit
+//
+//  Created by Ankit Kumar on 10/10/15.
+//  Copyright © 2015 Ankit Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestGit/TestGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestGit
+//
+//  Created by Ankit Kumar on 10/10/15.
+//  Copyright © 2015 Ankit Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
